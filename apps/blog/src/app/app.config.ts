5aa171c901d3a1369dd1815f3b87b3dc40f338a9
Justin Rassier <jrassier@adventhp.com>
2023-06-27 11:05:16 -0500

    add analog

---
+++ apps/blog/src/app/app.config.ts
@@ -0,0 +1,12 @@
+import { provideFileRouter } from '@analogjs/router';
+import { provideHttpClient } from '@angular/common/http';
+import { ApplicationConfig } from '@angular/core';
+import { provideClientHydration } from '@angular/platform-browser';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideFileRouter(),
+    provideClientHydration(),
+    provideHttpClient(),
+  ],
+};
