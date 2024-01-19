b2e3fcd7f8ac1617d9cf844c1ef035cb1a6b96b7
Justin Rassier <justinrassier@hey.com>
2024-01-19 06:31:23 -0600

    add link back to posts

---
+++ apps/blog/src/app/ideas/i-hate-ruby-so-why-am-i-attracted-to-rails.md
@@ -0,0 +1,4 @@
+I hate Ruby. But I think Rails is so intriguing. I can't stop reading about it
+
+- Watched rails documentary
+- Rails world videos
--- apps/blog/src/app/pages/blog/posts.[slug].page.ts
+++ apps/blog/src/app/pages/blog/posts.[slug].page.ts
@@ -2,13 +2,33 @@ import { MarkdownComponent, injectContent } from '@analogjs/content';
 import { AsyncPipe, DatePipe, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
+import { RouterLink } from '@angular/router';
 import { Post } from '../../data/posts';
 
 @Component({
   selector: 'post',
   standalone: true,
-  imports: [MarkdownComponent, AsyncPipe, NgIf, DatePipe],
+  imports: [MarkdownComponent, AsyncPipe, NgIf, DatePipe, RouterLink],
   template: `
+    <a class=" pt-2 pl-2 flex items-center gap-2" [routerLink]="'/blog'">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+          />
+        </svg>
+      </div>
+      <span>Back to posts</span>
+    </a>
     <div
       class="flex flex-grow justify-center min-h-screen"
       *ngIf="post$ | async as post"
