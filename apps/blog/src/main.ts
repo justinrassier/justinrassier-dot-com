b7f6ba0424eff7662927d649381a7d23602d9e6a
Justin Rassier <jrassier@adventhp.com>
2024-04-03 18:45:47 -0500

    add lua syntax highlighting

--- apps/blog/src/main.ts
+++ apps/blog/src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import 'zone.js';
 
+import 'prismjs';
+import 'prismjs/components/prism-lua';
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
